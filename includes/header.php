3ee3327b574b55818b585b5dd89edf3198e6a334
lorenzo-bnc <lorenzo.buonocoree@gmail.com>
2026-04-29 11:43:46 +0200

    second commit

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="developer" content="Lorenzo BUONOCORE">
+<meta name="copyright" content="Corealms <?= date('Y') ?>">
+<title>Corealms</title>
+<link rel="stylesheet" href="./assets/style.css">
+<link rel="icon" href="./IMG/logo.jpg">
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require './includes/header.php' ?>
+</head>
+<body>
+    <header>
+        <nav>
+            <div id="nav">
+                <img src="./IMG/logo.png" id="nav"/>
+                <ul>
+                    <li><a>Accueil</a></li>
+                    <li><a>Projets</a></li>
+                    <li><a>Nous</a></li>
+                    <li><a>Yzq</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div id="content">
+            <section id="home">
+                <div>
+
+                </div>
+            </section>
+
+            <section id="project">
+
+            </section>
+
+            <section id="us">
+
+            </section>
+            
+            <section id="home">
+                
+            </section>
+        </div>  
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
